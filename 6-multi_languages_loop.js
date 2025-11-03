f1f338a50d627586a7ff89a0d7f78154bc26425a
TAREK <tareksayedmokhtar@yahoo.com>
2025-11-03 22:35:52 +0200

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,12 +1,6 @@
-// 6-multi_languages_loop.js
 const langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let out = "";
-
 for (let i = 0; i < langs.length; i++) {
-  out += langs[i];
-  if (i !== langs.length - 1) {
-    out += "\n";
-  }
+  out += langs[i] + (i < langs.length - 1 ? "\n" : "");
 }
-
 console.log(out);
